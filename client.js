c0a12276c83e1a2e73be97ebb55306e46afa0044
Upgrayedd0408 <michaellowry89@gmail.com>
2023-12-16 05:14:45 +0000

    added constants.js file and made adjustments to IP and Port lines of connect function

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require("net");
+const {IP, port} = require("./constants");
 
 const connect = function () {
   const conn = net.createConnection({
-    host: "localhost",// IP address here,
-    port: 50541 // PORT number here,
+    IP,
+    port
   });
 
   conn.on("data", (data) => {
---
+++ constants.js
@@ -0,0 +1,13 @@
+const IP = "localhost"// IP address here,
+const port = 50541 // PORT number here,
+
+
+
+
+
+
+
+module.exports = {
+  IP,
+  port
+};
